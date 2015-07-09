a20e13bb734571438a5c4e8a4080d3c9a410e688
Rawr <rekked.noraa@gmail.com>
2015-07-10 01:26:03 +1200

    Restructured code to create objects onload.
    collision detection needs work on what happens at boundary values.

--- Control.js
+++ Control.js
@@ -1,3 +1,233 @@
+//================================================================================================
+//objects
+//================================================================================================
+
+function Scene() {
+	this.offset = 10
+	this.cellsize = 32 //cellsize
+	this.objectsArray = new MyArray() //objects array
+	this.context = null //context gets set in the onload function
+	this.w = 0 //width
+	this.h = 0 //height
+	this.collisionGranularity = 2
+}
+
+function User() {
+ 	this.curObj = null //current object
+	this.mouse = { //mouse
+		f: false // for mouse down and hold.
+	}
+}
+
+function Polygon(x, y, w, h) {
+
+	this.xPos = x
+	this.yPos = y
+	this.width = w
+	this.height = h
+
+	this.x = function(number) {
+		if (typeof number === "number") {
+			this.xPos = number
+		} else {
+			return this.xPos
+		} 
+	}
+
+	this.y = function(number) {
+		if (typeof number === "number") {
+			this.yPos = number
+		} else {
+			return this.yPos
+		} 
+	}
+
+	this.w = function(number) {
+		if (typeof number === "number") {
+			this.width = number
+		} else {
+			return this.width
+		} 
+	}
+
+	this.h = function(number) {
+		if (typeof number === "number") {
+			this.height = number
+		} else {
+			return this.height
+		} 
+	}
+
+	this.collides = function(obj) {
+		temp = this.findClosestPoints(obj)
+		a = temp[0]
+		b = temp[1]
+		c = temp[2]
+		d = temp[3]
+		return (this.xyIn(a, b) || obj.xyIn(c, d))
+	}
+
+	this.xyIn = function (x, y) {
+		numX = x
+		numY = y
+		if (this.height >= 0) {
+			//height is postive
+			if (this.width >= 0) {
+				//width is postive
+				if ((this.xPos <= numX) && (this.xPos + this.width >= numX)) {
+					if ((this.yPos <= numY) && (this.yPos + this.height >= numY)) {
+
+						return true
+					}
+				}
+			else
+				//width is negative
+				if ((this.xPos >= numX) && (this.xPos + this.width <= numX)) {
+					if ((this.yPos <= numY) && (this.yPos + this.height >= numY)) {
+						return true
+					}
+				}
+			}
+		} else {
+			//height is negative
+			if (this.width >= 0) {
+				//width is postive
+				if ((this.xPos <= numX) && (this.xPos + this.width >= numX)) {
+					if ((this.yPos >= numY) && (this.yPos + this.height <= numY)) {
+						return true
+					}
+				}
+			} else {
+				//width is negative
+				if ((this.xPos >= numX) && (this.xPos + this.width <= numX)) {
+					if ((this.yPos >= numY) && (this.yPos + this.height <= numY)) {
+						return true
+					}
+				}
+			}
+		}
+		return false
+	}
+
+	this.findClosestPoints = function(arg) {
+
+		shortestDistance = Infinity
+		points = []
+
+		for (var i = 0; i < 4; i++) {
+			if (i == 0) {
+			   	x1 = this.x()
+			   	y1 = this.y()
+			} else if (i == 1) {
+				x1 = this.x() + this.w()
+				y1 = this.y()
+			} else if (i == 2) {
+				x1 = this.x()
+				y1 = this.y() + this.h()
+			} else if (i == 3) {
+				x1 = this.x() + this.w()
+				y1 = this.y() + this.h()
+			}
+			for (var j = 0; j < 4; j++) {
+				if (j == 0) {
+					x2 = arg.x()
+					y2 = arg.y()
+				}else if (j == 1) {
+					x2 = arg.x() + arg.w()
+					y2 = arg.y()
+				} else if (j == 2) {
+					x2 = arg.x()
+					y2 = arg.y() + arg.h()
+				} else if (j == 3) {
+					x2 = arg.x() + arg.w()
+					y2 = arg.y() + arg.h()
+				}
+				a = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2))
+				if (a < shortestDistance) {
+					shortestDistance = a
+					points = [x2, y2, x1, y1]
+				}
+			}
+		}
+		return points
+	}
+
+	this.equal = function (obj) {
+		if (this.x() == obj.x()) {
+			if (this.y() == obj.y()) {
+				if (this.w() == obj.w()) {
+					if (this.h() == obj.h()) {
+						return true
+					}
+				}
+			}		
+		}
+		return false
+	}
+
+	this.toString = function() {
+		return this.xPos + ", " + this.yPos + "; " + this.width + ", " + this.height
+	}
+}
+
+function GreyBox(x, y, w, h) {
+	Polygon.call(this)
+	//I re-added these variables because I couldn't figure out how to pass the GreyBox function
+	//inputs in as Polygon function inputs
+
+	this.xPos = x
+	this.yPos = y
+	this.width = w
+	this.height = h
+
+	this.focus = true
+	this.index = null
+
+	this.toggleFocus = function() {
+		if (this.focus) {
+			this.focus = false
+		} else {
+			this.focus = true
+		}
+	}
+
+	this.draw = function(ctx) {
+		if (this.focus) {
+			ctx.fillStyle = "grey"
+		} else {
+			ctx.fillStyle = "black"
+		}
+		ctx.fillRect(this.xPos, this.yPos, this.width, this.height)
+	}
+}
+
+GreyBox.prototype = Object.create(Polygon.prototype)
+
+function CollBox(x, y, w, h) {
+	Polygon.call(this)
+
+	this.xPos = x
+	this.yPos = y
+	this.width = w
+	this.height = h
+
+	this.objRefs = []
+
+	this.removeIndex = function(arg) {
+		for (var i = 0; i < this.objRefs.length; i++) {
+			if (this.objRefs[i] == arg) {
+
+			} 
+		}
+	}
+	this.draw = function(ctx) {
+		ctx.fillStyle = "black"
+		ctx.fillRect(this.xPos, this.yPos, this.width, this.height)
+	}
+}
+
+CollBox.prototype = Object.create(Polygon.prototype)
+
 function MyArray(){
 	this.objects = []
 	this.freeIndices = []
@@ -66,78 +296,120 @@ function MyArray(){
 	}
 }
 
-var scene = {
-	offset: 10,
-	cellsize: 32,
-	objectsTable: {},
-	objectsArray: new MyArray(),
-	context: null //This gets set in the onload function
-}
+function Collision(arg) {
+	this.box = []
 
-var user = {
-	currentObj: null,
-	currentRef: null,
-	mouse: {
-		flag: false
+	num = arg.collisionGranularity
+	divW = arg.w / num
+	divH = arg.h / num
+
+	for (var i = 0; i < num; i++) {
+		for (var j = 0; j < num; j++) {
+			this.box[this.box.length] = new CollBox(j*divW, i*divH, divW, divH)
+		}
+	}
+	this.removeIndice = function(arg) {
+		for (var i = 0; i < this.box.length; i++) {
+			this.box[i].removeIndex(arg)
+		}
+	}
+
+	this.getIndices = function(arg) {
+		x = []
+		y = []
+		for (var i = 0; i < this.box.length; i++) {
+			if (this.box[i].collides(arg)) {
+				x = x.concat(this.box[i].objRefs)
+				y = y.concat([i])
+			}
+		}
+		return [x, y]
+	}
+	this.check = function(arg) {
+		x = this.getIndices(arg)
+		objectIndices = x[0]
+		//console.log("indices: ", objectIndices)
+		nums = x[1]
+		//console.log("box: ", nums)
+		if (scene.objectsArray.length() > 0) {
+			for (var i = 0; i < objectIndices.length; i++) {
+				if (scene.objectsArray.getObject(objectIndices[i]).collides(arg)) {
+					return false
+				}
+			}
+		}
+		for (var i = 0; i < nums.length; i++) {
+			this.box[nums[i]].objRefs = this.box[nums[i]].objRefs.concat([arg.index])
+			//console.log(nums[i], this.box[nums[i]].objRefs)
+		}
+		return true
 	}
 }
 
-function draw_background(){
-	var ctx = scene.context
+function Draw(arg) {
+	//wants to write a couple of properties from the scene object
+	var ctx = arg.context
+	var cell = arg.cellsize
+	var divWidth = Math.ceil(canvas_1.width/cell)
+	var divHeight = Math.ceil(canvas_1.height/cell)
 
-	var dividedWidth = canvas_1.width/scene.cellsize
-	var dividedHeight = canvas_1.height/scene.cellsize
+	this.background = function() {
+		ctx.fillStyle = "white"
+		ctx.fillRect(0, 0, canvas_1.width, canvas_1.height)
 
-	ctx.fillStyle = "white"
-	ctx.fillRect(0, 0, canvas_1.width, canvas_1.height)
-
-	ctx.strokeStyle = "#38e1ff"
-	ctx.strokeWidth = 5
-	ctx.beginPath()
-	for (var i = 0; i < dividedWidth; i++) {
-		ctx.moveTo(i*scene.cellsize, 0)
-		ctx.lineTo(i*scene.cellsize, canvas_1.height)
+		ctx.strokeStyle = "#38e1ff"
+		ctx.strokeWidth = 5
+		ctx.beginPath()
+		for (var i = 0; i < divWidth; i++) {
+			ctx.moveTo(i * cell, 0)
+			ctx.lineTo(i * cell, canvas_1.height)
+		}
+		for (var i = 0; i < divHeight; i++) {
+			ctx.moveTo(0, i * cell)
+			ctx.lineTo(canvas_1.width, i * cell)
+		}
+		ctx.stroke()
 	}
-	for (var i = 0; i < dividedHeight; i++) {
-		ctx.moveTo(0, i*scene.cellsize)
-		ctx.lineTo(canvas_1.width, i*scene.cellsize)
+
+	this.object = function(arg) {
+		arg.draw(ctx)
 	}
-	ctx.stroke()
 }
 
-function keyboardMouseSetup() {
-	var xPos, yPos
+//================================================================================================
+//functions
+//================================================================================================
+
+function keyboardMouseSetup(arg) {
+	var x, y
+	var cell = arg.cellsize
+	var off = arg.offset
+
 	$("#canvas_1").mousedown(function(data) {
-		user.mouse.flag = true
-		xPos = Math.floor((data.pageX - scene.offset) / scene.cellsize)
-		yPos = Math.floor((data.pageY - scene.offset) / scene.cellsize)
+		user.mouse.f = true
+		x = Math.floor((data.pageX - off) / cell)
+		y = Math.floor((data.pageY - off) / cell)
+		x1 = x * cell
+		y1 = y * cell
 
-		if (scene.objectsTable[[xPos, yPos]] == 1) {
-			if (user.currentObj) {
-				user.currentObj.toggleFocus()
+		obj = new GreyBox(x1, y1, cell, cell)
+		obj.index = scene.objectsArray.nextRef()
+
+		check = collision.check(obj)
+		if (check) {
+			if (user.curObj) {
+				user.curObj.toggleFocus()
 			}
-			var obj = scene.objectsArray.linearSearch([xPos, yPos])
-			obj[0].toggleFocus()
-			user.currentObj = obj[0]
-			user.currentRef = obj[1]
-		} else {
-			if (user.currentObj) {
-				user.currentObj.toggleFocus()
-			}
-
-			var x = new blackBox(xPos, yPos)
-			user.currentObj = x
-			user.currentRef = scene.objectsArray.nextRef()
-			scene.objectsArray.addObject(x)
-
-			scene.objectsTable[[xPos, yPos]] = 1
+			user.curObj = obj
+			scene.objectsArray.addObject(obj)
 		}
 	})
 
 	$("#canvas_1").mousemove(function(data) {
-		if (user.mouse.flag) {
-			if (user.currentObj) {
-				//
+		if (user.mouse.f) {
+			if (user.curObj) {
+				//this will just be moving the currently selected object.
+				//fine movement until mouseup at which point, snap to box
 			} else {
 				return
 			}
@@ -145,93 +417,46 @@ function keyboardMouseSetup() {
 	})
 
 	$(document).mouseup(function(data) {
-		user.mouse.flag = false
+		user.mouse.f = false
 	})
 
 	$(document).keydown(function(data) {
-		if (user.currentObj) {
-			scene.objectsArray.removeObject(user.currentRef)
-			user.currentRef = null
-			user.currentObj = null
+		if (user.curObj) {
+			scene.objectsArray.removeObject(user.curObj.index)
+			collision.removeIndice(user.curObj.index)
+			user.curObj = null
 		}
 	})
 }
 
-var blackBox = function(sX, sY){
-	var ctx = scene.context
-	var c = scene.cellsize
-	var sX = sX 	//start X
-	var sY = sY 	//start Y
-	var w = 1
-	var h = 1
-	var focus = true
-
-	this.draw = function(){
-		if (focus) {
-			ctx.fillStyle = "grey"
-		} else {
-			ctx.fillStyle = "black"
-		}
-		ctx.fillRect(c*sX, c*sY, c*w, c*h)
-	}
-
-	this.update = function(uX, uY){
-		sX = uX
-		sY = uY
-	}
-
-	this.toggleFocus = function() {
-		if (focus) {
-			focus = false
-		} else {
-			focus = true
-		}
-	}
-
-	this.equal = function(args) {
-		if (args.length > 2) {
-			return false
-		} else {
-			if (args[0] == sX) {
-				if (args[1] == sY) {
-					return true
-				}
-			}
-			return false
-		}
-	}
-}
-
-function setupTableValues(){
-	var x = canvas_1.width / scene.cellsize
-	var y = canvas_1.height / scene.cellsize
-	for (var i = 0; i < x; i++) {
-		for (var j = 0; j < y; j++) {
-			scene.objectsTable[[i, j]] = 0
-		}
-	}
-}
+//================================================================================================
+//runtime
+//================================================================================================
 
 onload = function () {
+	scene = new Scene()
 	scene.context = document.getElementById("canvas_1").getContext("2d")
-	keyboardMouseSetup()
-	setupTableValues()
+	scene.w = document.getElementById("canvas_1").width
+	scene.h = document.getElementById("canvas_1").height
 
-	x = new BlueBox(8, 10, 3, 2)
-	y = new BlueBox(10, 9, 3, 4)
-	console.log(x.toString())
-	console.log(y.toString())
-	console.log(x.col(y))
-	console.log(y.col(x))
+	collision = new Collision(scene)
+
+	user = new User()
+	draw = new Draw(scene)
+	
+	keyboardMouseSetup(scene)
 
 	setInterval(function(){
-		draw_background()
+		draw.background()
 
+		//for (var i = 0; i < collision.box.length; i++) {
+		//	draw.object(collision.box[i])
+		//}
 		if (scene.objectsArray.length() > 0) {
 			for (var i = 0; i < scene.objectsArray.length(); i++) {
 				var x = scene.objectsArray.getObject(i)
 				if (x) {
-					x.draw()
+					draw.object(x)
 				}
 			}
 		}
---
+++ MathsExtras.js
@@ -0,0 +1,15 @@
+function Fraction(args) {
+	return "Not yet implemented"
+}
+
+function Matrix(args) {
+	return "Not yet implemented"
+}
+
+function Function(args) {
+	return "Not yet implemented"
+}
+
+function Graph(args) {
+	return "Not yet implemented"
+}
